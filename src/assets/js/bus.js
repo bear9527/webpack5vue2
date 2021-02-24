8f765c9f7e77f2c830fc4e047e1935579aa4db54
mxf <530144289@qq.com>
2021-02-24 18:01:06 +0800

    edit bus.js

--- src/assets/js/bus.js
+++ src/assets/js/bus.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 const Bus = new Vue()
-let aaab = new Promise((resolve,reject)=>{
-    resolve("d")
-  }).then((str)=>{
-    alert(str)
-  })
-  console.log(aaab)
+
 Vue.prototype.$bus = Bus;
 Object.defineProperty(Vue.prototype, '$bus',{
     writable:false
